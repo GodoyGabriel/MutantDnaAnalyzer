17d50ad225cd50a108fb17e9d798cfab4b317e43
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2021-02-16 13:59:33 -0300

    Update configSV.js

--- src/config/configSV.js
+++ src/config/configSV.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 const configSV = {
-    hostname: process.env.HOSTNAME_SV,
-    port: process.env.HOSTNAME_PORT
+    hostname: process.env.HOSTNAME_SV || 'localhost',
+    port: process.env.HOSTNAME_PORT || 4000
 }
 
 module.exports = { configSV };
