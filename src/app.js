df6b9bdb9f714e65733d0bdd606f28f1182530b8
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2021-02-15 22:27:26 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
-// Cargar modulos de node para crear servidor
 const express = require('express');
 const bodyParser = require('body-parser');
-
-// Ejecutar express (http)
 const app = express();
-
-// Cargar ficheros rutas
 const dnaRoutes = require('./routes/dnaRoutes');
 
 // Middlewares
-app.use(bodyParser.urlencoded({ extended: false} ));   // Para utilizar el bodyparser
-app.use(bodyParser.json());  // Convertir las peticiones a json
+app.use(bodyParser.urlencoded({ extended: false} ));
+app.use(bodyParser.json()); 
 
-// CORS
-
-// Añadir prefijos a rutas / Cargar rutas
+// Routes
 app.use('/', dnaRoutes);
-// Exportar modulo (fichero actual)
+
 module.exports = app;
