12c95a8482868e5e8439e87117b2df368e5b6045
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2021-02-16 15:43:17 -0300

    test heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const { configSV } = require('./config/configSV');
+require('dotenv').config();
 require(('./services/dbConnection'));
 const app = require('./app');
 const logger = require('./utils/logger');
 
 app.listen(configSV.port,() => {
   logger.info('****Welcome to X-Men analyze DNA Api****');
-  console.log('config', configSV.hostname)
+  console.log('config', process.env)
   logger.info(`Server running on the port ${configSV.port}`);
 }); 
