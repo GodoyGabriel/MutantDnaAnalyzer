c20cdc70adaa5e57f109d83ba47e846b754e1b7f
Godoy Gabriel <58715211+GodoyGabriel@users.noreply.github.com>
2021-02-16 15:56:42 -0300

    Add HEROKU_URI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const { configSV } = require('./config/configSV');
-require('dotenv').config();
-require(('./services/dbConnection'));
-const app = require('./app');
-const logger = require('./utils/logger');
+const { configSV } = require("./config/configSV");
+require("dotenv").config();
+require("./services/dbConnection");
+const app = require("./app");
+const logger = require("./utils/logger");
 
-app.listen(configSV.port,() => {
-  logger.info('****Welcome to X-Men analyze DNA Api****');
-  console.log('config', process.env.npm_config_production)
-  logger.info(`Server running on the port ${configSV.port}`);
-}); 
+app.listen(configSV.port, () => {
+  logger.info("****Welcome to X-Men analyze DNA Api****");
+  const uriSV = process.env.npm_config_production
+    ? process.env.HEROKU_URI
+    : `http://${configSV.hostname}:${configSV.port}`;
+  logger.info(`Server running at ${uriSV}`);
+});
